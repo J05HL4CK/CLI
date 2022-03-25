376c764cb9c14693c2209f82776cfc2d6e4d6233
J05HL4CK <joshualack994@gmail.com>
2022-03-26 00:38:12 +1000

    pseudocode and most rough code for welcome msg and user options

--- rough.rb
+++ rough.rb
@@ -1,19 +1,41 @@
 def budget_app
+    # start heading
+    puts "---------------------------------------------------------------"
     puts %Q{Welcome to CHEEP, the little budget app for your command line}
     puts "---------------------------------------------------------------"
+    # end heading
+
     # ask user how they would like to continue
     puts "cheep! cheep!"
     puts "Let's get started!"
-    puts "How would you like to continue?, Please select one of the following options"
+    puts "How would you like to continue?"
        
     # give user a list of options to choose from
-    # budget tool 
-    # savings realised
-    # compare
-    # goals
+    user_options = [
+        {:option => "Budget", :selection => 1},
+        {:option => "Savings", :selection => 2},
+        {:option => "Compare", :selection => 3},
+        {:option => "Goals", :selection => 4}
+    ]
 
-    #get user option
-    #show user what they asked 
+    # display options to user
+    user_options.each do |option|
+        puts "#{option[:selection]}. #{option[:option]} "
+    end
+
+    # user to enter numeric value stating their selection from options 
+    usr_opt_select = gets.chomp.to_i
+
+    # confirm selection to user
+    puts "You have selected #{usr_opt_select}"
+    
+    #check if value is in the options array?
+    #if valid selection cont
+    #if not valid selection return to options menu 
+
+  
+
+
 
 end
 budget_app
