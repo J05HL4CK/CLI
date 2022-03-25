ca00ef8aec14d7f273c859f066d0a67c82df34a6
J05HL4CK <joshualack994@gmail.com>
2022-03-26 09:48:23 +1000

    case added for user input ( main options menu) [revisit this, got to be a better way with less code]

--- rough.rb
+++ rough.rb
@@ -11,7 +11,7 @@ def budget_app
     puts "How would you like to continue?"
        
     # give user a list of options to choose from
-    user_options = [
+    menu_options = [
         {:option => "Budget", :selection => 1},
         {:option => "Savings", :selection => 2},
         {:option => "Compare", :selection => 3},
@@ -19,17 +19,35 @@ def budget_app
     ]
 
     # display options to user
-    user_options.each do |option|
+    menu_options.each do |option|
         puts "#{option[:selection]}. #{option[:option]} "
     end
 
     # user to enter numeric value stating their selection from options 
     usr_opt_select = gets.chomp.to_i
+    # query user input, does it match a menu option
+    case usr_opt_select
+    when usr_opt_select = 1
+        puts "you have selected Budget tool"          
+        
+    when usr_opt_select = 2
+        puts "You have selected Savings tool"
+
+    when usr_opt_select = 3
+        puts "You have selected Comparison tool"   
+    when usr_opt_select = 4 
+        puts  "You have selected Goals tool"
+        
+    else
+        puts "Invalid selection, please try again"
+    end
 
     # confirm selection to user
-    puts "You have selected #{usr_opt_select}"
-    
+    #puts "You have selected #{usr_opt_select}"
+
     #check if value is in the options array?
+
+    
     #if valid selection cont
     #if not valid selection return to options menu 
 
