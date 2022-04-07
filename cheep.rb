46f9014c993973ecf14f8708d9f1d230a2cc8259
J05HL4CK <joshualack994@gmail.com>
2022-04-07 14:31:47 +1000

    user data class added for collecting inputs

--- cheep.rb
+++ cheep.rb
@@ -1,7 +1,38 @@
 require 'colorize' # colour background and font 
-require_relative("./heads_class") # headings and invalid warniing (maybe use tty for invalids)
+require_relative("./heads_class") # headings and invalid warning (maybe use tty for invalids)
 require "tty-prompt" # menus and prompts
-#-----Cheep Bugdet app Start-----
+# User data class
+class UserData
+    attr_accessor :data_category, :inputs
+    @@n_entries = 0
+# specify the data savings goals etc with data_cat
+    def initialize(data_category)
+       @data_category = data_category
+       @inputs = []
+       @@n_entries += 1
+               
+    end
+    # user to add category(home, transport etc) item(rent, pets etc) how often in days
+    def add(category, item, amount, frequency)
+        @inputs << {label: category, n: amount, days: frequency}
+
+    end
+    # delete the last entry
+    def delete_last
+        @inputs.pop
+    end
+    
+    def display_all
+        puts "Data Category: #{@data_category.capitalize} User Entries: #{@inputs.length}"
+        @inputs.each { | entry | puts "\n\t #{entry[:label]} $#{entry[:n]}  #{entry[:days]}."}
+        puts "\n\n#{@data_category.capitalize} Input Total: $#{@inputs.sum {|h|h[:n]}} "    
+    end
+    def to_s
+        return "Data category: #{@data_category.capitalize},\tEntries: #{@inputs.length}"
+
+    end
+end
+#-----Cheep Budget app Start-----
 def cheep
     prompt = TTY::Prompt.new
     # display welcome message to user - first point of contact for user
@@ -11,8 +42,12 @@ def cheep
     menu = prompt.select("To continue, please choose one of the following options: ", %w(Budget Savings Goals Help))
     # if the menu choice is comparable, do that thing
     case menu
+    # user has selected Budget, run budget 
     when "Budget"
-        puts "yes"
+        # budget heading 
+        Headings.budget
+
+
     when "Savings"
         puts "yes"
     when "Goals"
