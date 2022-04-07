f69a96f3b38613258cdad17715abb3f195cc3b25
J05HL4CK <joshualack994@gmail.com>
2022-04-07 15:44:44 +1000

    user prompt for name and assigned variable

--- cheep.rb
+++ cheep.rb
@@ -3,17 +3,22 @@ require_relative("./heads_class") # headings and invalid warning (maybe use tty 
 require "tty-prompt" # menus and prompts
 # User data class
 class UserData
+    # read and write access to categories and inputs 
     attr_accessor :data_category, :inputs
+    # initialise number of entries 
     @@n_entries = 0
-# specify the data savings goals etc with data_cat
+# specify the data buget/ savings/ goals etc with data_cat
     def initialize(data_category)
        @data_category = data_category
+       # storage array for user inputs
        @inputs = []
+       # plus 1 for every entry that's added 
        @@n_entries += 1
                
     end
     # user to add category(home, transport etc) item(rent, pets etc) how often in days
     def add(category, item, amount, frequency)
+        # push all into the storage array @inputs
         @inputs << {label: category, n: amount, days: frequency}
 
     end
@@ -21,12 +26,13 @@ class UserData
     def delete_last
         @inputs.pop
     end
-    
+    # display category, amount of entries and all entries 
     def display_all
         puts "Data Category: #{@data_category.capitalize} User Entries: #{@inputs.length}"
         @inputs.each { | entry | puts "\n\t #{entry[:label]} $#{entry[:n]}  #{entry[:days]}."}
         puts "\n\n#{@data_category.capitalize} Input Total: $#{@inputs.sum {|h|h[:n]}} "    
     end
+    # overide string to provide meaningful data when called
     def to_s
         return "Data category: #{@data_category.capitalize},\tEntries: #{@inputs.length}"
 
@@ -35,19 +41,22 @@ end
 #-----Cheep Budget app Start-----
 def cheep
     prompt = TTY::Prompt.new
+    name = TTY::Prompt.new
     # display welcome message to user - first point of contact for user
     Headings.welcome
-
+    # prompt for user name and assign to user_name variable
+    user_name = name.ask("Start by adding a user name", default: ENV["USER"])
     # prompt user to select an option to continue
     menu = prompt.select("To continue, please choose one of the following options: ", %w(Budget Savings Goals Help))
     # if the menu choice is comparable, do that thing
     case menu
-    # user has selected Budget, run budget 
+    #-----start Budget----- 
     when "Budget"
         # budget heading 
         Headings.budget
 
 
+    #-----end Budget
     when "Savings"
         puts "yes"
     when "Goals"
