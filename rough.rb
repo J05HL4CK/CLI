609cb2290c9d1415c609585ab74898c14cd9eb98
J05HL4CK <joshualack994@gmail.com>
2022-04-10 15:32:28 +1000

    module for budget calcs created in rough.rb

--- rough.rb
+++ rough.rb
@@ -1,14 +1,27 @@
-class PromptGets
-    def initialize
-        #name of the question or prompt
-        @name = name
-        #user response to prompt
-        @response = response
-        @inputs = []         
-    end
-    def select_option
-        puts "Please select one of the following options: "
-    end
-    def 
+module BudgetFunctions
 
+    def daily(amount)
+        @n1 = amount
+        @daily = @n1 / 365
+        puts @daily
+    end
+
+    def weekly(amount)
+        @n1 = amount
+        @weekly = @n1 / 52.0
+        puts @weekly
+    end
+    def fortnightly(amount)
+        @n1 = amount
+        @fortnightly = @n1 / 26.07
+        puts @fortnightly
+    end
+    def monthly(amount)
+        @n1 = amount
+        @monthly = @n1 / 12
+    end
+    def yearly(amount)
+        @n1 = amount
+        @monthly = @n1 / 
+    end
 end
