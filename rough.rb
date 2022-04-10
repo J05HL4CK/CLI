33b6d91b72307437e0c3c875665919eaa8d81c12
J05HL4CK <joshualack994@gmail.com>
2022-04-10 15:57:33 +1000

    budget math module salary/52 working

--- rough.rb
+++ rough.rb
@@ -1,27 +1,29 @@
 module BudgetFunctions
 
-    def daily(amount)
+    def BudgetFunctions.daily(amount)
         n1 = amount
         daily = n1 / 365
         puts daily
     end
 
-    def weekly(amount)
+    def BudgetFunctions.weekly(amount)
         n1 = amount
         weekly = n1 / 52.0
         puts weekly
     end
-    def fortnightly(amount)
+    def BudgetFunctions.fortnightly(amount)
         n1 = amount
         fortnightly = n1 / 26.07
         puts fortnightly
     end
-    def monthly(amount)
+    def BudgetFunctions.monthly(amount)
         n1 = amount
         monthly = n1 / 12
     end
-    def yearly(amount)
-        n1 = amount
-        monthly = n1 / 
-    end
+    # def BudgetFunctions.yearly(amount)
+        # n1 = amount
+        # monthly = n1 / 
+    # end
 end
+
+BudgetFunctions.weekly(26000)
