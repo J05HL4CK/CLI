b93523ced4b5dae6a2a5a5d889c7ca5ab18170d8
J05HL4CK <joshualack994@gmail.com>
2022-04-10 15:43:35 +1000

    removed instance variables from module

--- rough.rb
+++ rough.rb
@@ -1,27 +1,27 @@
 module BudgetFunctions
 
     def daily(amount)
-        @n1 = amount
-        @daily = @n1 / 365
-        puts @daily
+        n1 = amount
+        daily = n1 / 365
+        puts daily
     end
 
     def weekly(amount)
-        @n1 = amount
-        @weekly = @n1 / 52.0
-        puts @weekly
+        n1 = amount
+        weekly = n1 / 52.0
+        puts weekly
     end
     def fortnightly(amount)
-        @n1 = amount
-        @fortnightly = @n1 / 26.07
-        puts @fortnightly
+        n1 = amount
+        fortnightly = n1 / 26.07
+        puts fortnightly
     end
     def monthly(amount)
-        @n1 = amount
-        @monthly = @n1 / 12
+        n1 = amount
+        monthly = n1 / 12
     end
     def yearly(amount)
-        @n1 = amount
-        @monthly = @n1 / 
+        n1 = amount
+        monthly = n1 / 
     end
 end
