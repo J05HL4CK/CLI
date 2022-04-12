7bfadbc362803062fab61013dd185b9b099f4072
J05HL4CK <joshualack994@gmail.com>
2022-04-12 12:46:24 +1000

    start page added to headings class - heads_class.rb

--- sussudio.rb
+++ sussudio.rb
@@ -133,48 +133,4 @@ require "tty-prompt"
 #     end
 # end
 require "colorize"
-def yeet
 
-    puts " Created by Joshua Lack                                                             ".colorize(:color => :magenta, :background => :light_cyan)
-    puts " L337 BUDG37 4PP 4 N3RD5                                                                        ".colorize(:color => :light_cyan, :background => :light_cyan)
-    
-    puts "              ..-==\"\"\"\"==__                                                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              /              \\                                                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "          ___/   <0)          \\                                                    ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "       _=\"___\\                |                                                   ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "        =__  /                |                                                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "           ~~\\_              /___                                                  ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              \"/      ,.=\"~~~\"==_\"\"=__                                         ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              /^ ^  .\" \\ \\ \\ \\ \\ \"=_  \"\"=_                                 ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              | ^ ^ |\"-_ \\ \\ \\ \\ \\ \=_    \"\"=_                              ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "               \\ ^  \___\"-_\\ \\ \\ \\ \\ \=_      \"=_                           ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                \\ ^ ^\\ _____\\_\\_\\_\\_\\_\\_\"=_      \"=_                      ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                 \\ ^  ^\ -_-_-_-_-_-_-_-_-_\"=_      \"=_                          ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                  \\^ ^  \"-=_ -_-_-_-_-_-_-_-_-\"\"=__    \"=_                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                   \\\"=_ ^  ^ \"-_ -_-_-_-_-_-_-_-_-_-\"=_    \"=_                 ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                      \\\"=_^     ~~\"\"-===_______________\\      \"=.___________  ".colorize(:color => :light_yellow, :background => :light_cyan)  
-    puts "                         \"\"=_      -----______           _______\\_------- /      ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                             \"\"=-_______      \"\"\"\________=. \\  -_ -.\"-- /   ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                              // -- /   ~~~~--_   |         \". \  -_ ~- /          ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                               \\ \\  \\    ______\"\"\"            \"=_\\  \"-./   ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                                  \\ ----- -------\"               -_\\  /          ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                                  // --- /                         - /              ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                                   \\ \\  \\                           ~            ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                                      \\ \\                                         ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "                /$$$$$$  /$$   /$$ /$$$$$$$$ /$$$$$$$$ /$$$$$$$                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "               /$$__  $$| $$  | $$| $$_____/| $$_____/| $$__  $$                    ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              | $$  \\__/| $$  | $$| $$      | $$      | $$  \\ $$                  ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              | $$      | $$$$$$$$| $$$$$   | $$$$$   | $$$$$$$/                    ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              | $$      | $$__  $$| $$__/   | $$__/   | $$____/                     ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              | $$    $$| $$  | $$| $$      | $$      | $$                          ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "              |  $$$$$$/| $$  | $$| $$$$$$$$| $$$$$$$$| $$                          ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts "               \\______/ |__/  |__/|________/|________/|__/                         ".colorize(:color => :light_yellow, :background => :light_cyan)
-    puts " Thanks so much for downloading my app. I hope you find it useful!                                                                                     .".colorize(:color => :light_cyan, :background => :light_cyan)
-    puts " If you have any ideas for how CHEEP could be improved please feel free to contribute!                                      .".colorize(:color => :light_cyan,:background => :light_cyan)
-    puts " Version 1.0                                      Source githublink                 ".colorize(:color => :magenta, :background => :light_cyan)                                               
-
-                                                    
-                                                    
-
-end
-yeet
