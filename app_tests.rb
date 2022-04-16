5e7ef8d4994a8b5be6f2cc150667dec298a611c5
J05HL4CK <joshualack994@gmail.com>
2022-04-16 23:02:34 +1000

    more rubocopping

--- app_tests.rb
+++ app_tests.rb
@@ -1,16 +1,18 @@
-loop do 
-    user_expenses = expense.collect do
-    key(:category).ask("Start by adding a category name for your expenses: ", default: "Home" )
 
-    while prompt.yes?("Add an entry to this category?")
-    key(:item).values do
-        key(:name).ask("Enter a name for the expense: ", required: true)
-        key(:cost).ask("Enter the cost of payments $", required: true, convert: :float,)
+
+loop do
+  user_expenses = expense.collect do
+    key(:category).ask('Start by adding a category name for your expenses: ', default: 'Home')
+
+    while prompt.yes?('Add an entry to this category?')
+      key(:item).values do
+        key(:name).ask('Enter a name for the expense: ', required: true)
+        key(:cost).ask('Enter the cost of payments $', required: true, convert: :float)
         # (7,14,30,90,365)
-        key(:freq).ask("Enter the payment frequency in days: ", required: true, default: 7, validate: /(7|14|30|90|365)/ )
+        key(:freq).ask('Enter the payment frequency in days: ', required: true, default: 7,
+                                                                validate: /(7|14|30|90|365)/)
+      end
     end
+    break if yeet.keypress('Press esc to enter a new category', keys: [:escape])
+  end
 end
-break if yeet.keypress("Press esc to enter a new category", keys: [:escape])
-end
-end
-
