6622a460601d7b243c45c57b00afc56cddf413ec
J05HL4CK <joshualack994@gmail.com>
2022-04-16 22:45:54 +1000

    rubocopped it, got arrested for illegal indenting

--- budget_functions.rb
+++ budget_functions.rb
@@ -1,34 +1,41 @@
+# frozen_string_literal: true
+
 module BudgetFunctions
-    # 1/365 days a yr
-    def BudgetFunctions.daily(amount)
-        n1 = amount
-        daily = n1 / 365.25
-        puts daily
-    end
-    # 1/52 weeks a year
-    def BudgetFunctions.weekly(amount)
-        n1 = amount
-        weekly = n1 / 52.0
-        puts weekly
-    end
-    # 1/26.07th of a year is a fortnight
-    def BudgetFunctions.fortnightly(amount)
-        n1 = amount
-        fortnightly = n1 / 26.07
-        puts fortnightly
-    end
-    # 1/12th of a year = 1 month
-    def BudgetFunctions.monthly(amount)
-        n1 = amount
-        monthly = (1.0 / 12) * n1
-    end
-    # keep it simple!
-    def BudgetFunctions.difference(a, b)
-        c = a - b
-    end
-    def BudgetFunctions.product(a, b)
-        c = a * b
-    end
+  # 1/365 days a yr
+  def self.daily(amount)
+    n1 = amount
+    daily = n1 / 365.25
+    puts daily
+  end
+
+  # 1/52 weeks a year
+  def self.weekly(amount)
+    n1 = amount
+    weekly = n1 / 52.0
+    puts weekly
+  end
+
+  # 1/26.07th of a year is a fortnight
+  def self.fortnightly(amount)
+    n1 = amount
+    fortnightly = n1 / 26.07
+    puts fortnightly
+  end
+
+  # 1/12th of a year = 1 month
+  def self.monthly(amount)
+    n1 = amount
+    monthly = (1.0 / 12) * n1
+  end
+
+  # keep it simple!
+  def self.difference(a, b)
+    c = a - b
+  end
+
+  def self.product(a, b)
+    c = a * b
+  end
 end
 
-BudgetFunctions.weekly(26000)
+BudgetFunctions.weekly(26_000)
