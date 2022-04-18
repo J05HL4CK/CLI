182c1d6ff6a11629dd23ba88f26e409e73f947b4
J05HL4CK <joshualack994@gmail.com>
2022-04-18 15:10:32 +1000

    comments and pseudocode added, heading towards summary of data

--- app_tests.rb
+++ app_tests.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require 'tty-prompt'
-
+# ------------------------TTY INITIALISE------------------------
 income = TTY::Prompt.new
 expense = TTY::Prompt.new
 prompt = TTY::Prompt.new
 data_container = []
+# ------------------------START INCOME----------------------------------------
 user_income = income.collect do
   key(:salary).ask('Enter your annual net salary', convert: :float, required: true)
 
@@ -17,7 +18,9 @@ user_income = income.collect do
     key(:amount).ask('Enter the annual amount', required: true)
   end
 end
+# -------------------------END INCOME-----------------------------------------
 
+#--------------------------START EXPENSES-------------------------------------
 user_expenses = expense.collect do
   key(:category).ask('Start by adding a category name for your expenses: ', default: 'Home')
 
@@ -29,6 +32,7 @@ user_expenses = expense.collect do
                                                             validate: /(7|14|30|90|365)/)
   end
 end
-
+# ------------------------END EXPENSES---------------------------------------
 data_container.push(user_income, user_expenses)
 pp data_container
+# now I have the data.. so I need a total income, and total cost of expenses. i need to sort or map the data so freqs are the same before summing
